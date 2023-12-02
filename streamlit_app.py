0ba158b9f4b92f8336f656ad8109bee1b19e9f4b
Wajihullah <137669991+waji07@users.noreply.github.com>
2023-12-02 17:36:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,9 @@ sl.text('🥑🍞 Avocado Test')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 sl.dataframe(my_fruit_list)
